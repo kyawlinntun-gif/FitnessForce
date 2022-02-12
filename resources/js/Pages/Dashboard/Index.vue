7b8c25b2a879fb5e64a317eeae029b0adf08430d
kyawlinntun <kyawlinntun100@gmail.com>
2022-02-12 09:21:15 +0630

    00 Introduction - A single page application with Laravel using Inertia JS and Vue JS - YouTube, and 01 Laravel with Inertia JS set up for a new project - YouTube

---
+++ resources/js/Pages/Dashboard/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        This is dashboard, welcome {{ fname }}, {{ lname }}.
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Dashboard.Index',
+    props: ['fname', 'lname']
+}
+</script>
+
+<style>
+
+</style>
